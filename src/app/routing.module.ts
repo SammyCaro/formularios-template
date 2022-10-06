627ae5d0c0c213641de7316c6b2e86109171e133
SammyCaro <sammy.caro94@gmail.com>
2022-10-06 16:39:03 -0300

    Se añaden los componentes para los modulos reactive y template. Se añaden las rutas para cada modulo mencionado. Se añade las rutas hijas al modulo principal de rutas

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'reactive',
+    loadChildren: () =>
+      import('./reactive/reactive.module').then((m) => m.ReactiveModule),
+  },
+  {
+    path: 'template',
+    loadChildren: () =>
+      import('./template/template.module').then((m) => m.TemplateModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'template',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
