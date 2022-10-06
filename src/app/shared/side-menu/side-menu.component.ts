b09cdc410454f38220527786218b69541141350f
SammyCaro <sammy.caro94@gmail.com>
2022-10-06 16:58:51 -0300

    Se agrega navegacion al sidemenu tanto para template forms como para reactive forms

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface menuItem {
+  name: string;
+  path: string;
+}
 
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent implements OnInit {
-  constructor() {}
+export class SideMenuComponent {
+  templateMenuItems: menuItem[] = [
+    { name: 'Básicos', path: './template/basicos' },
+    { name: 'Dinámicos', path: './template/dinamicos' },
+    { name: 'Switches', path: './template/switches' },
+  ];
 
-  ngOnInit(): void {}
+  ReactiveMenuItems: menuItem[] = [
+    { name: 'Básicos', path: './reactive/basicos' },
+    { name: 'Dinámicos', path: './reactive/dinamicos' },
+    { name: 'Switches', path: './reactive/switches' },
+  ];
 }
