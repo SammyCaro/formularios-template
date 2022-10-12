2f3e3b06062243fe62ec9fa65e3d81d0fdba3859
SammyCaro <sammy.caro94@gmail.com>
2022-10-12 12:10:41 -0300

    Se añade HTML para el componente dinamicos del modulo template.Se agregan algunos inputs y se valida que el input nombre no este vacio

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
 })
 export class DinamicosComponent implements OnInit {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  validarNombre() {
+    return (
+      this.miFormulario?.controls['nombre'].invalid &&
+      this.miFormulario?.controls['nombre'].touched
+    );
+  }
+
+  guardar() {
+    console.log('formulario posteado');
+  }
 }
