e56f135aac3a974975bb3dcc7dd4941fe0ff5e2d
SammyCaro <sammy.caro94@gmail.com>
2022-10-12 13:47:31 -0300

    Se añade metodo para eliminar los juegos favoritos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,22 +1,45 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
   @ViewChild('miFormulario') miFormulario!: NgForm;
-
-  constructor() {}
-
-  ngOnInit(): void {}
+  persona: Persona = {
+    nombre: 'El roller',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Metal Gear',
+      },
+      {
+        id: 2,
+        nombre: 'Death Stranding',
+      },
+    ],
+  };
 
   validarNombre() {
     return (
-      this.miFormulario?.controls['nombre'].invalid &&
-      this.miFormulario?.controls['nombre'].touched
+      this.miFormulario?.controls['nombre']?.invalid &&
+      this.miFormulario?.controls['nombre']?.touched
     );
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
   guardar() {
