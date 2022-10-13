cf4e8e801e9bde410ff18f9598d2e3c703ffefa9
SammyCaro <sammy.caro94@gmail.com>
2022-10-13 15:24:52 -0300

    Se añade un formArray para que muestre elementos de formulario de manera dinamica. Esto se logra creando el formArray en el formBuilder, hacer un metodo getter para obtener ese array para que en el html pueda ser iterado

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,11 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
 })
-export class DinamicosComponent implements OnInit {
-  constructor() {}
+export class DinamicosComponent {
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.formBuilder.array(
+      [
+        ['Metal Gear', Validators.required],
+        ['Death Stranding', Validators.required],
+      ],
+      Validators.required
+    ),
+  });
 
-  ngOnInit(): void {}
+  /* get favoritos */
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  campoNoValido(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 }
