6999eee0dbe1bee896079f64355932af4537a733
SammyCaro <sammy.caro94@gmail.com>
2022-10-13 10:54:36 -0300

    Se crea y asocia el mismo formulario pero utilizando formBuilder

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -5,11 +5,17 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
   templateUrl: './basicos.component.html',
 })
 export class BasicosComponent {
-  miFormulario: FormGroup = new FormGroup({
+  /*   miFormulario: FormGroup = new FormGroup({
     nombre: new FormControl('RTX 3080'),
     precio: new FormControl(1500),
     existencias: new FormControl(4),
+  }); */
+
+  miFormulario: FormGroup = this.formBuilder.group({
+    nombre: ['RTX 3080'],
+    precio: [0],
+    existencias: [0],
   });
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 }
