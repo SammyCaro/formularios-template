899feb482d527b8125b7e4ed915c982f18d17a4f
SammyCaro <sammy.caro94@gmail.com>
2022-10-13 12:26:31 -0300

    Se añade el submit del formulario basico reactivo. En el método que hace el submit se valida que el formulario este bien antes de hacer envío de la informacion. Si la informacion se envia bien, el formulario se resetea

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
 })
-export class BasicosComponent {
+export class BasicosComponent implements OnInit {
   /*   miFormulario: FormGroup = new FormGroup({
     nombre: new FormControl('RTX 3080'),
     precio: new FormControl(1500),
@@ -19,10 +19,29 @@ export class BasicosComponent {
 
   constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    /* si le ponemos setValue hay que tener todos los valores, sino la app revienta
+      TAMBIEN SE PUEDE HACER CON .RESET() */
+    this.miFormulario.setValue({
+      nombre: 'RTX 3080',
+      precio: 1500,
+      existencias: 4,
+    });
+  }
+
   campoNoValido(campo: string) {
     return (
-      this.miFormulario.controls[campo]?.invalid &&
-      this.miFormulario.controls[campo]?.touched
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
     );
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
   }
 }
